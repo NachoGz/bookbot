8899032ebb30144bd9807d3ddae32b2915ff5f2b
Ignacio Gomez <ignaciogomezf1@gmail.com>
2025-07-21 20:21:15 -0300

    change format of report, added argument option

--- main.py
+++ main.py
@@ -1,72 +1,32 @@
 
 import os
-
-# def count_words(text: str) -> int:
-#     words = text.split()
-
-#     return len(words)
-
-
-# def count_characters(text: str) -> dict:
-#     frequency = {}
-#     for ch in text:
-#         if ch.isalpha():
-#             ch = ch.lower()
-#             if ch not in frequency:
-#                 frequency[ch] = 1
-#             else:
-#                 frequency[ch] += 1
-
-#     return frequency
-
-
-# def main():
-#     books = os.listdir("books/")
-#     book = input("What book would you like to analyze? (Use the name of the file in books/): ") + ".txt"
-    
-#     while book not in books:
-#         print("Book not found... Try again!")
-#         print()
-#         book = input("What book would you like to analyze? (Use a name in books/): ") + ".txt"
-
-#     with open(f"books/{book}") as f:
-#         text = f.read()
-
-#     amnt_words = count_words(text)
-
-#     amnt_char = count_characters(text)
-
-#     print(f"--- Begin report of books/{book} ---")
-#     print(f"{amnt_words} words found in the document")
-#     print()
-
-#     for k,v in amnt_char.items():
-#         print(f"The '{k}' character was found {v} times")
-
-#     print("--- End report ---")
+from stats import get_num_words
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    	
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    print()
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
 
     for item in chars_sorted_list:
         if not item["char"].isalpha():
             continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+        print(f"{item['char']}: {item['num']}")
 
-    print("--- End report ---")
+    print("============= END ===============")
 
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 
 def sort_on(d):
